3b84935ae362d2ebd3eec3d7cc101f7e2e972250
Uzair <Uzair>
2016-12-10 11:16:35 -0500

    gear ratio 1, added experiment class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SynergyAutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SynergyAutoBlue.java
@@ -39,34 +39,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
-/**
- * This file illustrates the concept of driving a path based on encoder counts.
- * It uses the common Pushbot hardware class to define the drive on the robot.
- * The code is structured as a LinearOpMode
- *
- * The code REQUIRES that you DO have encoders on the wheels,
- *   otherwise you would use: PushbotAutoDriveByTime;
- *
- *  This code ALSO requires that the drive Motors have been configured such that a positive
- *  power command moves them forwards, and causes the encoders to count UP.
- *
- *   The desired path in this example is:
- *   - Drive forward for 48 inches
- *   - Spin right for 12 Inches
- *   - Drive Backwards for 24 inches
- *   - Stop and close the claw.
- *
- *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
- *  that performs the actual movement
- *  This methods assumes that each movement is relative to the last stopping place.
- *  There are other ways to perform encoder based moves, but this method is probably the simplest.
- *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
-
-@Autonomous(name="SynergyAutoFinalBlu", group="Autonomous")
+@Autonomous(name="SynergyAutoBlue", group="Autonomous")
 
 public class SynergyAutoBlue extends LinearOpMode {
 
@@ -75,7 +48,7 @@ public class SynergyAutoBlue extends LinearOpMode {
     private ElapsedTime     runtime = new ElapsedTime();
 
     static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
-    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
     static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
                                                       (WHEEL_DIAMETER_INCHES * 3.14159265);
@@ -111,10 +84,11 @@ public class SynergyAutoBlue extends LinearOpMode {
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
+        launch();
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
         encoderDrive(DRIVE_SPEED,  33,  33, 5.0);
-        encoderDrive(TURN_SPEED,   6, -6, 4.0);
+        turn();
         encoderDrive(DRIVE_SPEED,  13, 13, 9.0);
         encoderDrive(DRIVE_SPEED,  0,  0, 10.0);
         encoderDrive(DRIVE_SPEED,  14,  14, 3.0);
@@ -130,6 +104,14 @@ public class SynergyAutoBlue extends LinearOpMode {
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
+    }
+
+    protected void turn() throws InterruptedException {
+        encoderDrive(TURN_SPEED,   6, -6, 4.0);
+    }
+
+    protected void launch() throws InterruptedException {
+        //do nothing
     }
 
     /*
